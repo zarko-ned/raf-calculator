f37ecf80f27fb5b7298470c2bc7cc286adbb76dd
Zarko <zarkoned@outloo.com>
2025-10-04 14:05:31 +0200

    calculator

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(5, 3);
+        System.out.println("The sum is: " + result);
+    }
+}
