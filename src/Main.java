af6c05bf427ceb3ad549bf9df620bb81c01952cc
sofiascepanovic <sscepanovic15423ri@raf.rs>
2025-10-16 21:09:51 +0200

    added mutlipy method

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,5 @@ public class Main {
         int result = calculator.add(5, 3);
         System.out.println("The sum is: " + result);
 
-        int result2 = calculator.subtract(10, 4);
-        System.out.println("The difference is: " + result2);
-
-        int result3 = calculator.multiply(6, 7);
-        System.out.println("The product is: " + result3);
-
-        int result4 = calculator.divide(20, 4);
-        System.out.println("The quotient is: " + result4);
     }
 }
