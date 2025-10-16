4ac83c58df79c66b679774f22bf8dfa63ea5a131
sofiascepanovic <sscepanovic15423ri@raf.rs>
2025-10-16 21:09:01 +0200

    added subtract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,18 +3,6 @@ public class Calculator {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
+    public int subtract(int a, int b) {return a - b;}
 
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Division by zero is not allowed.");
-        }
-        return a / b;
-    }
 }
